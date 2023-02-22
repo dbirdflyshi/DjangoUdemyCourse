4d5844ebe3f38ef08635d46d99d8b833fde7365d
Dane Anderson <dane.anderson@chep.com>
2023-02-22 13:50:53 -0500

    complete level 5

--- Level 5/learning_users/learning_users/settings.py
+++ Level 5/learning_users/learning_users/settings.py
@@ -127,13 +127,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [STATIC_DIR,]
 
 # MEDIA
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+# LOGIN
+LOGIN_URL = '/basic_app/user_login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
